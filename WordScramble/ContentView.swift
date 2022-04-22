0c672cac2b955004e861f1f740e3551c51687514
swiftJules <101522134+swiftJules@users.noreply.github.com>
2022-04-22 07:46:33 -0400

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by RBS70 on 4/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by RBS70 on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
